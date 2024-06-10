7c20a443cf497a6054017c4781c442c3561804f8
Sâmya Clemente <samya.clemente07@gmail.com>
2024-06-10 13:54:23 -0300

    update

---
+++ src/main/java/com/example/candidato/CadastroDeCandidatoApplication.java
@@ -0,0 +1,20 @@
+package com.example.candidato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class CadastroDeCandidatoApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CadastroDeCandidatoApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeCandidatoApplication.class, args);
+	}
+}
